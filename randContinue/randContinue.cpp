901032ec550c74dceb20ef06738a25defe3945f2
dubious.way <dubiousway@gmail.com>
2017-07-20 15:54:58 +0900

    [randContinue] added program  parameters

--- randContinue/randContinue.cpp
+++ randContinue/randContinue.cpp
@@ -3,26 +3,30 @@
 #include <math.h>
 #include <time.h>
 
+#define RATESNUM 4
 class Rand01{
-	static int m_ranges[3][2];
-	static double m_rates[];
+	static int m_ranges[RATESNUM][2];
+	static double m_rates[RATESNUM];
 
 	double m_current;
 
 	int randRateWithRangeIndex(){
-		int rangeWithInt[3];
+		int ratesWithInt[RATESNUM];
 		int total = 0;
-		for (int i=0;i<3;i++){
-			rangeWithInt[i] = int(m_rates[i]*100);
-			total += rangeWithInt[i];
+		for (int i=0;i<RATESNUM;i++){
+			ratesWithInt[i] = int(m_rates[i]*100);
+			total += ratesWithInt[i];
 		}
 		int r = random() % total;
-		if(r < rangeWithInt[0])
+		int sumRates = ratesWithInt[0];
+		if(r < sumRates)
 			return 0;
-		else if(r >= rangeWithInt[0] + rangeWithInt[1])
+		else if(r >= sumRates && r < (sumRates+=ratesWithInt[1]))
+			return 1;
+		else if(r >= sumRates && r < (sumRates+=ratesWithInt[2]))
 			return 2;
 		else
-			return 1;
+			return 3;
 	}
 	//double rand_001(int* srange){
 	//	int r = random() % (srange[1] - srange[0]) + srange[0];
@@ -33,7 +37,7 @@ class Rand01{
 		int* rng = m_ranges[rangeIndex];
 		int r = random() % (rng[1] - rng[0]) + rng[0];
 		int pm = random()%2;
-		double r2 = r*0.0001;
+		double r2 = r*0.001;
 		return pm ? r2 : -r2;
 	}
 
@@ -52,12 +56,20 @@ public:
 	}
 };
 
-int Rand01::m_ranges[3][2] = {{0,5},{5,8},{8,10}};
-double Rand01::m_rates[] = {0.5,0.35,0.15};	// 10:7:3
+int Rand01::m_ranges[4][2] = {{0,5},{5,8},{8,10},{10,15}};
+double Rand01::m_rates[] = {0.5,0.35,0.10,0.05};	// 10:7:2:1
 
-int main(){
-	Rand01* r = new Rand01(80.12);
-	r->save2file(1000);
+int main(int argc, char* argv[]){
+	double  initval = 80.12;
+	int		ticknum = 1000;
+// parameters processing
+	if (argc > 1)
+		ticknum = atoi(argv[1])	;
+	if (argc > 2)
+		initval = atof(argv[2])	;
+	
+	Rand01* r = new Rand01(initval);
+	r->save2file(ticknum);
 
 	return 0;
 }
